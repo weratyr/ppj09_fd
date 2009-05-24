4e72721cdf452cdb13ab6bf62729814cfb36c343
Admin <Admin@laebbi>
2009-05-24 13:53:45 +0200

    UserRateForm, UserRateView, Rate implementiert so weit es geht.
    Es fehlt noch: Benuter der Bewertet und der BEwertet werden soll -> session
    Die Impl Klassen Methode -> SwapManagerImpl.java

--- src/ppj09/gwt/swapweb/client/serverInterface/SearchHandlerAsync.java
+++ src/ppj09/gwt/swapweb/client/serverInterface/SearchHandlerAsync.java
@@ -1,5 +1,5 @@
 /*
- * @(#)SearchHandlerAsync.java      			 20.04.09
+ * @(#)SearchManager.java      			 20.04.09
  *
  * Copyright (c) 2008-2009 Project Team 4711
  * All rights reserved.
@@ -8,22 +8,21 @@
 package ppj09.gwt.swapweb.client.serverInterface;
 
 import java.util.ArrayList;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import ppj09.gwt.swapweb.client.datatype.ArticleSearchQuery;
 import ppj09.gwt.swapweb.client.datatype.ArticleSearchResult;
 import ppj09.gwt.swapweb.client.datatype.SearchQuery;
 import ppj09.gwt.swapweb.client.datatype.SearchResult;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
- * SearchHandlerAsync Interface fuer den RPC des SearchHandlerImpl. Das
- * Interface enthaelt die Methoden, die auf der Serverseite via
- * <b>AsyncCallback</b> aufgerufen werden.
+ * SearchHandler Interface ist fuer den RPC des SearchHandlerImpl. Das Interface
+ * enthaelt die Methoden, die auf der Serverseite aufgerufen werden.
  * 
  * @author Christian Happ
  * @author Projekt Team 4711
  * @version 0.1, 04.05.09
  */
 public interface SearchHandlerAsync {
-	public void search(ArticleSearchQuery s, AsyncCallback<ArrayList<SearchResult>> callback);
+	public void search(ArticleSearchQuery searchQuery, AsyncCallback<ArrayList<SearchResult>> callback);
 }
