3f4db4da22281b2bb83cf2daabe5304797bf7cbf
geo <geo@192.168.178.156>
2009-05-25 22:26:24 +0200

    UserRegistrationForm neu

--- src/ppj09/gwt/swapweb/client/gui/UserRegistrationForm.java
+++ src/ppj09/gwt/swapweb/client/gui/UserRegistrationForm.java
@@ -7,32 +7,20 @@
 
 package ppj09.gwt.swapweb.client.gui;
 
-import java.util.ArrayList;
-
 import ppj09.gwt.swapweb.client.Validation;
 import ppj09.gwt.swapweb.client.datatype.User;
 import ppj09.gwt.swapweb.client.serverInterface.UserManager;
 import ppj09.gwt.swapweb.client.serverInterface.UserManagerAsync;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FocusListenerAdapter;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
-import com.gwtext.client.widgets.MessageBox;
-import com.gwtext.client.widgets.Panel;
-import com.gwtext.client.widgets.Window;
-import com.gwtext.client.widgets.form.Field;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.gwtext.client.widgets.form.FormPanel;
+import com.gwtext.client.widgets.form.MultiFieldPanel;
 import com.gwtext.client.widgets.form.TextField;
-import com.gwtext.client.widgets.form.event.FieldListenerAdapter;
-import com.gwtext.client.widgets.form.event.TextFieldListenerAdapter;
+import com.gwtext.client.widgets.form.NumberField;
+import com.gwtext.client.widgets.layout.ColumnLayoutData;
 
 /**
  * Formularfelder und Submit des Benutzers zur Registrierung. Implementiert das
@@ -44,189 +32,64 @@ import com.gwtext.client.widgets.form.event.TextFieldListenerAdapter;
  */
 public class UserRegistrationForm extends Composite implements Form {
 	private final UserManagerAsync userManager = GWT.create(UserManager.class);
-	private Label lblFirstName;
-	private AbsolutePanel absolutePanel;
-	private Label lblLastName;
-	private Label lblZip_City;
-	private Label lblStreet_Number;
+	private HorizontalPanel horizontalPanel;
+	private FormPanel formPanel_1;
+	private MultiFieldPanel namePanel, cityPanel;
+	private NumberField txtbxZip;
+	private TextField txtbxCity;
+	private FormPanel formPanel;
+	private TextField txtbxUserName;
+	private TextField txtbxLastName;
 	private TextField txtbxFirstName;
-	private TextField txtbLastName;
-	private TextField txtbZip;
-	private TextField txtbCity;
-	private TextField txtbStreet;
-	private TextField txtbNumber;
-	private Label lblUserName;
-	private TextField txtbUsername;
-	private TextField txtbEmail;
-	private TextField txtbPassword;
-	private TextField txtbPasswordRepeat;
-	private Label lblPassword;
-	private Label lblPasswordRepeat;
-	private Label lblEmail;
-	private Label lblEmailRepeat;
-	private TextField txtbEmailRepeat;
-	private VerticalPanel verticalPanel;
-	private Label usernameFreeLabel;
-	private Panel errorPanel;
-	private Window window;
-	private Button registration;
 
 	/**
 	 * Initialisiert Formular Eingabefelder
 	 */
 	public UserRegistrationForm() {
-
 		{
-			verticalPanel = new VerticalPanel();
-			absolutePanel = new AbsolutePanel();
-			verticalPanel.add(absolutePanel);
-			initWidget(verticalPanel);
-			absolutePanel.setSize("650", "325px");
+			horizontalPanel = new HorizontalPanel();
+			initWidget(horizontalPanel);
 			{
-				lblFirstName = new Label("Vorname:");
-				absolutePanel.add(lblFirstName, 33, 3);
-				lblFirstName
-						.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-			}
-			{
-				lblLastName = new Label("Nachname:");
-				absolutePanel.add(lblLastName, 23, 28);
-			}
-			{
-				lblZip_City = new Label("PLZ/ Wohnort:");
-				absolutePanel.add(lblZip_City, 5, 54);
-			}
-			{
-				lblStreet_Number = new Label("Stra\u00DFe/ Nr:");
-				absolutePanel.add(lblStreet_Number, 25, 80);
-			}
-			{
-				txtbxFirstName = new TextField();
-				txtbxFirstName.setAllowBlank(false);
+				formPanel_1 = new FormPanel();
 
-				absolutePanel.add(txtbxFirstName, 94, 0);
-				txtbxFirstName.addListener(new TextFieldListenerAdapter() {
-					public void onBlur(Field field) {
-						System.out.println("onChange: changed to ");
-					}
-				});
+				txtbxFirstName = new TextField("Vor / Nachname", "text_field",
+						110);
 
-			}
-			{
-				txtbLastName = new TextField();
-				txtbLastName.setAllowBlank(false);
-				absolutePanel.add(txtbLastName, 94, 26);
+				txtbxLastName = new TextField("Nachname", "text_field", 110);
+				txtbxLastName.setHideLabel(true);
 
-			}
-			{
-				txtbZip = new TextField();
-				txtbZip.setAllowBlank(false);
-				absolutePanel.add(txtbZip, 94, 52);
-				txtbZip.setSize("50", "22");
+				namePanel = new MultiFieldPanel();
+				namePanel.addToRow(txtbxFirstName, 240);
+				namePanel.addToRow(txtbxLastName, new ColumnLayoutData(1));
+				namePanel.setBorder(false);
+				formPanel_1.add(namePanel);
 
-			}
-			{
-				txtbCity = new TextField();
-				txtbCity.setAllowBlank(false);
-				absolutePanel.add(txtbCity, 147, 52);
-				txtbCity.setSize("106", "22");
+				txtbxZip = new NumberField("PLZ / Wohnort", "number_field", 60);
+				txtbxZip.setAllowBlank(false);
 
-			}
-			{
-				txtbStreet = new TextField();
-				txtbStreet.setAllowBlank(false);
-				absolutePanel.add(txtbStreet, 94, 76);
-				txtbStreet.setWidth("96");
+				
+				txtbxCity = new TextField("Wohnort", "text_field", 160);
+				txtbxCity.setHideLabel(true);
+				txtbxCity.setAllowBlank(false);
 
+				
+				cityPanel = new MultiFieldPanel();
+				cityPanel.addToRow(txtbxZip, 190);
+				cityPanel.addToRow(txtbxCity, new ColumnLayoutData(1));
+				cityPanel.setBorder(false);
+				formPanel_1.add(cityPanel);
+				
+				horizontalPanel.add(formPanel_1);
 			}
 			{
-				txtbNumber = new TextField();
-				txtbNumber.setAllowBlank(false);
-				absolutePanel.add(txtbNumber, 193, 76);
-				txtbNumber.setWidth("60");
-
+				formPanel = new FormPanel();
+				{
+					txtbxUserName = new TextField("Benutzername", "text_field",
+							100);
+					formPanel.add(txtbxUserName);
+				}
+				horizontalPanel.add(formPanel);
 			}
-			{
-				lblUserName = new Label("Benutzername:");
-				absolutePanel.add(lblUserName, 331, 3);
-			}
-
-			{
-				txtbUsername = new TextField();
-				txtbUsername.setAllowBlank(false);
-
-				// überprüfen ob der Benutzername noch frei ist. Wenn nein,
-				// setze "usernameFreeLabel".
-				txtbUsername.addListener(new FieldListenerAdapter() {
-					public void onBlur(Field field) {
-						if (!usernameFree(txtbUsername.getText())) {
-							usernameFreeLabel
-									.setText("Benutzername schon vergeben");
-						}
-					}
-				});
-				absolutePanel.add(txtbUsername, 424, 0);
-			}
-			{
-				txtbPassword = new TextField();
-				txtbPassword.setAllowBlank(false);
-				absolutePanel.add(txtbPassword, 424, 26);
-
-			}
-			{
-				txtbPasswordRepeat = new TextField();
-				txtbPasswordRepeat.setAllowBlank(false);
-				absolutePanel.add(txtbPasswordRepeat, 424, 52);
-
-			}
-			{
-				txtbEmail = new TextField();
-				txtbEmail.setAllowBlank(false);
-				absolutePanel.add(txtbEmail, 424, 78);
-
-			}
-			{
-				lblPassword = new Label("Password:");
-				absolutePanel.add(lblPassword, 358, 28);
-			}
-			{
-				lblPasswordRepeat = new Label("Password Wiederholen:");
-				absolutePanel.add(lblPasswordRepeat, 283, 54);
-			}
-			{
-				lblEmail = new Label("E-Mail:");
-				absolutePanel.add(lblEmail, 378, 81);
-			}
-			{
-				lblEmailRepeat = new Label("E-Mail Wiederholen:");
-				absolutePanel.add(lblEmailRepeat, 303, 107);
-			}
-			{
-				txtbEmailRepeat = new TextField();
-				txtbEmailRepeat.setAllowBlank(false);
-				absolutePanel.add(txtbEmailRepeat, 424, 104);
-
-			}
-			{
-				registration = new Button();
-				absolutePanel.add(registration, 424, 159);
-				registration.setText("Registrieren");
-				registration.addClickHandler(new ClickHandler() {
-					public void onClick(ClickEvent event) {
-
-						submit();
-					}
-				});
-				registration.setSize("140", "25");
-			}
-			{
-				usernameFreeLabel = new Label();
-				absolutePanel.add(usernameFreeLabel, 579, 3);
-			}
-
-			errorPanel = new Panel();
-			errorPanel.setAutoScroll(true);
-			window = new Window();
 		}
 
 	}
@@ -259,79 +122,78 @@ public class UserRegistrationForm extends Composite implements Form {
 	 * Überprüft die Eingegebenen Daten. Fehlt ein oder mehrere Einträge, wird
 	 * dies in einem Popupfenster angezeigt.
 	 */
-	public void validate() {
-
-		ArrayList<String> errorList = new ArrayList<String>();
-		errorList.clear();
-
-		// Überprüfen ob die Felder leer sind.
-		if (txtbxFirstName.getText().equals("")) {
-			errorList.add("- Vorname eintragen");
-		}
-		if (txtbLastName.getText().equals("")) {
-			errorList.add("- Nachname eintragen");
-		}
-		if (txtbZip.getText().equals("")) {
-			errorList.add("- Postleitzahl eintragen");
-		}
-		if (txtbCity.getText().equals("")) {
-			errorList.add("- Wohnort eintragen");
-		}
-		if (txtbStreet.getText().equals("")) {
-			errorList.add("- Straße eintragen");
-		}
-		if (txtbNumber.getText().equals("")) {
-			errorList.add("- Hausnummer eintragen");
-		}
-		if (txtbUsername.getText().equals("")) {
-			errorList.add("- Benutzername eintragen");
-		}
-		if (txtbPassword.getText().equals("")) {
-			errorList.add("- Passwort eintragen");
-		}
-		if (txtbPasswordRepeat.getText().equals("")) {
-			errorList.add("- Passwort wiederholen");
-		}
-
-		// Prüfen ob Passwörter gleich sind
-		if (!txtbPassword.getText().equals(txtbPasswordRepeat.getText())) {
-			errorList.add("- Passwörter stimmen nicht überein");
-		}
-		if (txtbEmail.getText().equals("")) {
-			errorList.add("- eMail eintragen");
-		}
-		if (txtbEmailRepeat.getText().equals("")) {
-			errorList.add("- eMail wiederholen");
-		}
-
-		// Prüfen ob eMails gleich sind
-		if (!txtbEmail.getText().equals(txtbEmailRepeat.getText())) {
-			errorList.add("- eMail Adressen stimmen nicht überein");
-		}
-
-		// Erzeuge Popup wenn ein Fehler vorliegt
-		if (errorList.size() != 0) {
-			window.destroy();
-			if (window.isAttached()) {
-				errorList.clear();
-			}
-			window.clear();
-			window.setWidth(160);
-			window.setHeight(150);
-			window.add(errorPanel);
-			window.setCloseAction(Window.CLOSE);
-			window.setTitle("Fehler");
-			window.setAutoScroll(true);
-			for (int i = 0; i < errorList.size(); i++) {
-				Label error = new Label();
-				error.setText(errorList.get(i));
-				errorPanel.add(error);
-			}
-			window.show();
-		}
-
-	}
-
+	// public void validate() {
+	//
+	// ArrayList<String> errorList = new ArrayList<String>();
+	// errorList.clear();
+	//
+	// // Überprüfen ob die Felder leer sind.
+	// if (txtbxFirstName.getText().equals("")) {
+	// errorList.add("- Vorname eintragen");
+	// }
+	// if (txtbLastName.getText().equals("")) {
+	// errorList.add("- Nachname eintragen");
+	// }
+	// if (txtbZip.getText().equals("")) {
+	// errorList.add("- Postleitzahl eintragen");
+	// }
+	// if (txtbCity.getText().equals("")) {
+	// errorList.add("- Wohnort eintragen");
+	// }
+	// if (txtbStreet.getText().equals("")) {
+	// errorList.add("- Straße eintragen");
+	// }
+	// if (txtbNumber.getText().equals("")) {
+	// errorList.add("- Hausnummer eintragen");
+	// }
+	// if (txtbUsername.getText().equals("")) {
+	// errorList.add("- Benutzername eintragen");
+	// }
+	// if (txtbPassword.getText().equals("")) {
+	// errorList.add("- Passwort eintragen");
+	// }
+	// if (txtbPasswordRepeat.getText().equals("")) {
+	// errorList.add("- Passwort wiederholen");
+	// }
+	//
+	// // Prüfen ob Passwörter gleich sind
+	// if (!txtbPassword.getText().equals(txtbPasswordRepeat.getText())) {
+	// errorList.add("- Passwörter stimmen nicht überein");
+	// }
+	// if (txtbEmail.getText().equals("")) {
+	// errorList.add("- eMail eintragen");
+	// }
+	// if (txtbEmailRepeat.getText().equals("")) {
+	// errorList.add("- eMail wiederholen");
+	// }
+	//
+	// // Prüfen ob eMails gleich sind
+	// if (!txtbEmail.getText().equals(txtbEmailRepeat.getText())) {
+	// errorList.add("- eMail Adressen stimmen nicht überein");
+	// }
+	//
+	// // Erzeuge Popup wenn ein Fehler vorliegt
+	// if (errorList.size() != 0) {
+	// window.destroy();
+	// if (window.isAttached()) {
+	// errorList.clear();
+	// }
+	// window.clear();
+	// window.setWidth(160);
+	// window.setHeight(150);
+	// window.add(errorPanel);
+	// window.setCloseAction(Window.CLOSE);
+	// window.setTitle("Fehler");
+	// window.setAutoScroll(true);
+	// for (int i = 0; i < errorList.size(); i++) {
+	// Label error = new Label();
+	// error.setText(errorList.get(i));
+	// errorPanel.add(error);
+	// }
+	// window.show();
+	// }
+	//
+	// }
 	/*
 	 * Methode um zu Überprüfen ob der Benutzername noch frei ist.
 	 */
