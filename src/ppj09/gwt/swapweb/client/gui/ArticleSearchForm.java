8ba80419cd59599db9120b2d9fbac59ddeca56f1
weratyr <evil@killer.magdlos.com>
2009-06-02 13:59:41 +0200

    erste Änderungen zu gwtext bei ArticleSearchForm

--- src/ppj09/gwt/swapweb/client/gui/ArticleSearchForm.java
+++ src/ppj09/gwt/swapweb/client/gui/ArticleSearchForm.java
@@ -37,6 +37,19 @@ import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.gwtext.client.core.Position;
+import com.gwtext.client.data.SimpleStore;
+import com.gwtext.client.data.Store;
+import com.gwtext.client.widgets.Panel;
+import com.gwtext.client.widgets.form.ComboBox;
+import com.gwtext.client.widgets.form.Field;
+import com.gwtext.client.widgets.form.FormPanel;
+import com.gwtext.client.widgets.form.MultiFieldPanel;
+import com.gwtext.client.widgets.form.TextField;
+import com.gwtext.client.widgets.layout.AnchorLayoutData;
+import com.gwtext.client.widgets.layout.ColumnLayoutData;
+import com.gwtext.client.widgets.layout.FormLayout;
+import com.gwtext.client.widgets.form.Field;
 
 /**
  * Formularfelder und Submit der Artikelsuche. Implementiert das Interface Form.
@@ -47,7 +60,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
  */
 public class ArticleSearchForm extends Composite implements Form {
 	private TabPanel tabPanel;
-	private AbsolutePanel absolutePanel;
+	private FormPanel formPanel;
 	private Label searchqueryLabel;
 	private TextBox searchqueryTextBox;
 	private Label excludeArticleLabel;
@@ -79,155 +92,177 @@ public class ArticleSearchForm extends Composite implements Form {
 		searchPanel.setSpacing(8);
 		SearchtextBox = new TextBox();
 		searchPanel.add(SearchtextBox);
-		
+
 		categoryComboBoxQuckSearch = new ListBox();
 		searchPanel.add(categoryComboBoxQuckSearch);
 		categoryComboBoxQuckSearch.addItem("Kategorie");
 		categoryComboBoxQuckSearch.setSize("120", "");
 		container.add(searchPanel);
-		
+
 		SearchButton = new Button("Suche");
 		searchPanel.add(SearchButton);
 		SearchButton.setHeight("22");
 
-		advancedSearchHyperlink = new Hyperlink("New hyperlink",
-				false, "newHistoryToken");
+		advancedSearchHyperlink = new Hyperlink("New hyperlink", false,
+				"newHistoryToken");
 		searchPanel.add(advancedSearchHyperlink);
 		advancedSearchHyperlink.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				tabPanel.setVisible(!tabPanel.isVisible());
-				
+
 			}
 		});
 		advancedSearchHyperlink.setText("erweiterte Suche");
-		
-		
+
 		tabPanel = new TabPanel();
-		tabPanel.setVisible(false);
+		// tabPanel.setVisible(false);
 		container.add(tabPanel);
-		{
-			absolutePanel = new AbsolutePanel();
-			tabPanel.add(absolutePanel, "Artikelsuche", false);
-			tabPanel.selectTab(0);
-			absolutePanel.setSize("600px", "450");
 
-			{
-				searchqueryLabel = new Label("Suchbegriff oder Tauschnummer:");
-				absolutePanel.add(searchqueryLabel, 5, 5);
-				searchqueryLabel.setSize("218", "18");
-			}
-			{
-				searchqueryTextBox = new TextBox();
-				absolutePanel.add(searchqueryTextBox, 5, 23);
-				searchqueryTextBox.setWidth("218");
-			}
-			{
-				excludeArticleLabel = new Label(
-						"Begriffe von der Suche ausschlie\u00DFen:");
-				absolutePanel.add(excludeArticleLabel, 5, 48);
-			}
-			{
-				excludeArticleTextBox = new TextBox();
-				absolutePanel.add(excludeArticleTextBox, 5, 66);
-				excludeArticleTextBox.setSize("218", "");
-			}
-			{
-				articleLocationLabel = new Label("Artikelstandort:");
-				absolutePanel.add(articleLocationLabel, 5, 96);
-			}
-			{
-				articleLocationTextBox = new TextBox();
-				absolutePanel.add(articleLocationTextBox, 103, 93);
-				articleLocationTextBox.setWidth("120");
-			}
-			{
-				articleConditionLabel = new Label("Zustand:");
-				absolutePanel.add(articleConditionLabel, 42, 122);
-			}
-			{
-				articleConditionComboBox = new ListBox();
-				absolutePanel.add(articleConditionComboBox, 103, 120);
-				articleConditionComboBox.addItem("Beliebig");
-				articleConditionComboBox.addItem("Neu");
-				articleConditionComboBox.addItem("Gebraucht");
-				articleConditionComboBox.setWidth("120");
-			}
-			{
-				deliveryComboBox = new ListBox();
-				absolutePanel.add(deliveryComboBox, 103, 144);
-				deliveryComboBox.addItem("Beliebig");
-				deliveryComboBox.addItem("Postversand");
-				deliveryComboBox.addItem("Abholung");
-				deliveryComboBox.addItem("Treffen");
-				deliveryComboBox.setWidth("120");
-			}
-			{
-				deliveryLabel = new Label("Versandart:");
-				absolutePanel.add(deliveryLabel, 26, 145);
-			}
-			{
-				categoryComboBox = new ListBox();
-				absolutePanel.add(categoryComboBox, 230, 24);
-				categoryComboBox.addItem("Kategorie");
-				categoryComboBox.setSize("120", "");
-			}
-			{
-				activeArticleCheckBox = new CheckBox("New check box");
-				absolutePanel.add(activeArticleCheckBox, 387, 23);
-				activeArticleCheckBox.setHTML("Nur aktive Artikel Anzeigen");
-			}
-			{
-				pictureArticlesCheckBox = new CheckBox(
-						"Nur Artikel mit Bildern anzeigen");
-				absolutePanel.add(pictureArticlesCheckBox, 387, 45);
-			}
-			{
-				resultButton = new Button("New button");
-				absolutePanel.add(resultButton, 387, 138);
-				resultButton.addClickHandler(new ClickHandler() {
-					public void onClick(ClickEvent event) {
-						System.out.println("gedrückt");
-						/**
-						 * TODO erstellt aus den Formulardaten ein ArticleSearch
-						 * Objekt und übergibt es per RPC an
-						 * SearchHandler.search()
-						 */
-						SearchHandlerAsync searchHandler = GWT
-								.create(SearchHandler.class);
-						searchHandler.search(new ArticleSearchQuery(),
-								new AsyncCallback<ArrayList<SearchResult>>() {
-									public void onFailure(Throwable caught) {
-										System.out.println("neeee: ");
-									}
+		formPanel = new FormPanel();
+		formPanel.setLabelAlign(Position.TOP);
 
-									public void onSuccess(
-											ArrayList<SearchResult> results) {
-										System.out.println("neeee: ");
-										for (SearchResult r : results) {
-											searchResultPanel.add((Widget) r
-													.getView());
-										}
-									}
-								});
-					}
-				});
+		Panel firstColumn = new Panel();
+		firstColumn.setLayout(new FormLayout());
+		firstColumn.setBorder(false);
+		firstColumn.setPaddings(10);
 
-				resultButton.setText("Ergebnisse anzeigen");
+		firstColumn.add(new TextField("Suche",
+				"searchPhrase", 110));
+		firstColumn.add(new TextField("Begriff ignorieren",
+				"searchPhrase", 110));
+		firstColumn.add(new TextField("Artikelstandort", "searchPhrase", 110));
+
+		Panel secondColumn = new Panel();
+		secondColumn.setLayout(new FormLayout());
+		secondColumn.setBorder(false);
+		secondColumn.setPaddings(10);
+
+		Object[][] optionsCondition = new Object[][] {
+				new Object[] { "b", "Beliebig" }, new Object[] { "n", "Neu" },
+				new Object[] { "g", "Gebraucht" }, };
+
+		Store conditionStore = new SimpleStore(new String[] { "b", "options" },
+				optionsCondition);
+		conditionStore.load();
+
+		final ComboBox articleConditionCB = new ComboBox();
+		articleConditionCB.setFieldLabel("Artikel Zustand");
+		articleConditionCB.setStore(conditionStore);
+		articleConditionCB.setDisplayField("options");
+		articleConditionCB.setMode(ComboBox.LOCAL);
+		articleConditionCB.setTriggerAction(ComboBox.ALL);
+		articleConditionCB.setForceSelection(true);
+		articleConditionCB.setValueField("b");
+		articleConditionCB.setReadOnly(true);
+		articleConditionCB.setWidth(90);
+		secondColumn.add(articleConditionCB);
+
+		Object[][] optionsDelivery = new Object[][] {
+				new Object[] { "b", "Beliebig" },
+				new Object[] { "p", "Postversand" },
+				new Object[] { "a", "Abholung" },
+				new Object[] { "t", "Treffen" }, };
+
+		Store deliveryStore = new SimpleStore(new String[] { "b", "options" },
+				optionsDelivery);
+		conditionStore.load();
+
+		final ComboBox articleDeliveryCB = new ComboBox();
+		articleDeliveryCB.setFieldLabel("Versandart");
+		articleDeliveryCB.setStore(deliveryStore);
+		articleDeliveryCB.setDisplayField("options");
+		articleDeliveryCB.setMode(ComboBox.LOCAL);
+		articleDeliveryCB.setTriggerAction(ComboBox.ALL);
+		articleDeliveryCB.setForceSelection(true);
+		articleDeliveryCB.setValueField("b");
+		articleDeliveryCB.setReadOnly(true);
+		articleDeliveryCB.setWidth(90);
+		secondColumn.add(articleDeliveryCB);
+
+	
+		Object[][] optionsCategory = new Object[][] {
+				new Object[] { "index", "nix drin" }, };
+
+		Store categoryStore = new SimpleStore(new String[] { "b", "options" },
+				optionsCategory);
+		conditionStore.load();
+
+		final ComboBox articleCategoryCB = new ComboBox();
+		articleCategoryCB.setFieldLabel("Kategorie");
+		articleCategoryCB.setStore(categoryStore);
+		articleCategoryCB.setDisplayField("options");
+		articleCategoryCB.setMode(ComboBox.LOCAL);
+		articleCategoryCB.setTriggerAction(ComboBox.ALL);
+		articleCategoryCB.setForceSelection(true);
+		
+		articleCategoryCB.setReadOnly(true);
+		articleCategoryCB.setWidth(90);
+		secondColumn.add(articleCategoryCB);
+
+		deliveryLabel = new Label("Versandart:");
+		categoryComboBox = new ListBox();
+		// formPanel.add(categoryComboBox);
+		categoryComboBox.addItem("Kategorie");
+		categoryComboBox.setSize("120", "");
+
+		activeArticleCheckBox = new CheckBox("New check box");
+		// formPanel.add(activeArticleCheckBox);
+		activeArticleCheckBox.setHTML("Nur aktive Artikel Anzeigen");
+
+		pictureArticlesCheckBox = new CheckBox(
+				"Nur Artikel mit Bildern anzeigen");
+		// formPanel.add(pictureArticlesCheckBox);
+
+		resultButton = new Button("New button");
+		// formPanel.add(resultButton);
+		resultButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				System.out.println("gedrückt");
+				/**
+				 * TODO erstellt aus den Formulardaten ein ArticleSearch Objekt
+				 * und übergibt es per RPC an SearchHandler.search()
+				 */
+				SearchHandlerAsync searchHandler = GWT
+						.create(SearchHandler.class);
+				searchHandler.search(new ArticleSearchQuery(),
+						new AsyncCallback<ArrayList<SearchResult>>() {
+							public void onFailure(Throwable caught) {
+								System.out.println("neeee: ");
+							}
+
+							public void onSuccess(
+									ArrayList<SearchResult> results) {
+								System.out.println("neeee: ");
+								for (SearchResult r : results) {
+									searchResultPanel.add((Widget) r.getView());
+								}
+							}
+						});
 			}
-			{
-				resultLabel = new Label("5 Artikel gefunden");
-				absolutePanel.add(resultLabel, 387, 115);
-				resultLabel.setWidth("143");
-			}
-			{
-				searchResultPanel = new VerticalPanel();
-				absolutePanel.add(searchResultPanel, 5, 160);
-			}
-		}
+		});
+
+		resultButton.setText("Ergebnisse anzeigen");
+
+		resultLabel = new Label("5 Artikel gefunden");
+		// formPanel.add(resultLabel);
+		resultLabel.setWidth("143");
+
+		searchResultPanel = new VerticalPanel();
+		// formPanel.add(searchResultPanel);
+
+		MultiFieldPanel left = new MultiFieldPanel();
+		left.addToRow(firstColumn, 180);
+		left.addToRow(secondColumn, 180);
+
+		formPanel.add(left);
+
+		tabPanel.add(formPanel, "Artikelsuche", false);
+		tabPanel.selectTab(0);
+
 		{
 			userSearchForm = new UserSearchForm();
 			tabPanel.add(userSearchForm, "Benutzersuche", false);
-			userSearchForm.setSize("600px", "250");
+			userSearchForm.setSize("600px", "150");
 		}
 
 	}
