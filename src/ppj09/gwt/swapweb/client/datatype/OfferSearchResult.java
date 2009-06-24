95ad2c87688713a7c0f04aa3d3ea8ec06ff61c64
André Wagner <a.wagner87@gmail.com>
2009-06-24 13:37:52 +0200

    Bewertungsbutton eingefügt.

--- src/ppj09/gwt/swapweb/client/datatype/OfferSearchResult.java
+++ src/ppj09/gwt/swapweb/client/datatype/OfferSearchResult.java
@@ -19,6 +19,16 @@ public class OfferSearchResult implements SearchResult, Serializable {
 	private String offeredTo;
 	private boolean swapConcluded;
 	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	private User user;
+	
 	public boolean isSwapConcluded() {
 		return swapConcluded;
 	}
